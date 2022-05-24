2247c127732922d5d315ff682701554365c45ca1
vlyr <vlyr@tuta.io>
2022-05-24 20:10:25 +0300

    VGA

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,20 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::fmt::Write;
+
+mod vga;
+use vga::WRITER;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
 
     loop {}
 }
